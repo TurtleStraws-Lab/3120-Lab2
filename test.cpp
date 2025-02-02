36c4378186309a7f6fe5af6fe19687a1941d4d9a
TurtleStraws-Lab <gonzalez.moises532776@gmail.com>
2025-02-02 13:04:46 -0800

    my first commit

---
+++ test.cpp
@@ -0,0 +1,34 @@
+//Moises Gonzalez
+//2/2/2025
+//
+#include <iostream>
+#include <chrono>
+using namespace std;
+int sum1(int n){
+int total = 0;
+for(int i = 0; i <= n; i++)
+    total += i;
+return total;
+}
+int sum2(int n)
+{
+if(n == 1)
+    return 1;
+else
+    return sum2(n-1)+n;
+}
+
+int sum3(int n)
+{
+return n*(1+n)/2;
+}
+int main() {
+using chrono::high_resolution_clock;
+  int n = 100;
+auto start = chrono::high_resolution_clock::now();
+cout << "First Method: Sum of first " << n << " numbers is: " << sum1(n) << endl;
+auto end = chrono::high_resolution_clock::now();
+chrono::duration<double> duration = (end - start) * 100000;
+cout << "Runtime: " << duration.count() << " seconds" << endl;
+return 0;
+}
